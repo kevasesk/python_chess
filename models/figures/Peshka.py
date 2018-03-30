c8a49d65cb658227346bd98ce3243480c502e695
jeka <some@mail.com>
2018-03-30 23:31:21 +0200

    Improve Peshka

--- models/figures/Peshka.py
+++ models/figures/Peshka.py
@@ -2,6 +2,8 @@ from settings import *
 from models.figures.AbstractFigure import AbstractFigure
 
 class Peshka(AbstractFigure):
+    crossed = False
+
     def __init__(self, color):
         super(Peshka, self).__init__( color)
         self.color = color
@@ -16,22 +18,30 @@ class Peshka(AbstractFigure):
 
 
     def getAvaliableMoves(self, point, field):
-        if self.id == 'p': # white peshka rules
-            moves = [{'x':point['x'], 'y':point['y'] + 1}]
+        moves = []
+        if self.color == WHITE:
+            if not isinstance(field[point['x']][point['y'] + 1],AbstractFigure):
+                moves.append({'x':point['x'], 'y':point['y'] + 1}) # simple move
             if point['y'] == 1:
-                moves.append({'x':point['x'], 'y':point['y'] + 2})
-            if isinstance(field[point['x'] + 1][point['y'] + 1],AbstractFigure) and field[point['x'] + 1][point['y'] + 1].color == BLACK:
-                moves.append({'x': point['x'] + 1, 'y': point['y'] + 1})
-            if isinstance(field[point['x'] - 1][point['y'] + 1], AbstractFigure) and field[point['x'] - 1][ point['y'] + 1].color == BLACK:
-                moves.append({'x': point['x'] - 1, 'y': point['y'] + 1})
+                moves.append({'x':point['x'], 'y':point['y'] + 2}) # 2 cells move
+            if self.avaliableFight(field, BLACK, point['x'] + 1, point['y'] + 1 ):
+                moves.append({'x': point['x'] + 1, 'y': point['y'] + 1}) # fight right
+            if self.avaliableFight(field, BLACK, point['x'] - 1, point['y'] + 1 ):
+                moves.append({'x': point['x'] - 1, 'y': point['y'] + 1}) # fight left
             return moves
-        if self.id == 'P':# black peshka rules
-            moves = [{'x':point['x'], 'y':point['y'] - 1}]
+        if self.color == BLACK:
+            if not isinstance(field[point['x']][point['y'] - 1], AbstractFigure):
+                moves.append({'x': point['x'], 'y': point['y'] - 1})  # simple move
             if point['y'] == 6:
-                moves.append({'x':point['x'], 'y':point['y'] - 2})
-            if isinstance(field[point['x'] + 1][point['y'] - 1], AbstractFigure) and field[point['x'] + 1][point['y'] - 1].color == WHITE:
-                moves.append({'x': point['x'] + 1, 'y': point['y'] - 1})
-            if isinstance(field[point['x'] - 1][point['y'] - 1], AbstractFigure) and field[point['x'] - 1][point['y'] - 1].color == WHITE:
-                moves.append({'x': point['x'] - 1, 'y': point['y'] - 1})
+                moves.append({'x':point['x'], 'y':point['y'] - 2}) # 2 cells move
+            if self.avaliableFight(field, WHITE, point['x'] + 1, point['y'] - 1 ):
+                moves.append({'x': point['x'] + 1, 'y': point['y'] - 1}) # fight right
+            if self.avaliableFight(field, WHITE, point['x'] - 1, point['y'] - 1 ):
+                moves.append({'x': point['x'] - 1, 'y': point['y'] - 1}) # fight left
             return moves
         return []
+
+    def avaliableFight(self, field, color, x, y):
+        return ( self.inRange(x) and
+                 self.inRange(y) and
+                 isinstance(field[x][y],AbstractFigure) and field[x][y].color == color)
